3ff4088ea2c6890e91a4b4a4fd9299cc6d8f98f7
dandrichest <dandrichest@gmail.com>
2025-05-23 17:13:04 +0100

    week3

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
+import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+
+loadHeaderFooter();
+// Load the header and footer templates
+// and render them in the header and footer elements
 
 const dataSource = new ProductData("tents");
 
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,47 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false,
+) {
+  if (clear) parentElement.innerHTML = "";
+  const htmlStrings = list.map(templateFn);
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function renderWithTemplate(
+  template,
+  parentElement,
+  data,
+  callback
+) { 
+  parentElement.innerHTML = "template";
+  if (callback) {
+    callback(data);
+  }
+}
+export async function loadTemplate(path) {
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../patials/header.html");
+  const footerTemplate = await loadTemplate("../patials/footer.html");
+  const footerElement = document.querySelector("main-footer");
+  const headerElement = document.querySelector("main-header");
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
