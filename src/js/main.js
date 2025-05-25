f2e423e26dc04e5ab24136efce05afab0cb76984
dandrichest <dandrichest@gmail.com>
2025-05-25 20:16:02 +0100

    individual 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
-const tentsDataSource = new ProductData("tents");
-const tentsElement = document.querySelector(".product-list");
-const tentsList = new ProductList("tents", tentsDataSource, tentsElement);
-tentsList.init();
+loadHeaderFooter();
