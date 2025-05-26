0d9343d106d1f3f173b397f13caae08a2f4cb633
siaheguelable <siaheguelable@gmail.com>
2025-05-26 19:21:57 +0200

    Total$ in Cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,14 +1,18 @@
 import { getLocalStorage } from "./utils.mjs";
-
+ // this will be used to get the cart items from local storage
+// and render them in the cart page
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
   const productList = document.querySelector(".product-list");
   if (!cartItems.length) {
     productList.innerHTML = "<li>Your cart is empty.</li>";
+    document.getElementById("cartTotal").textContent = "0.00";
     return;
   }
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   productList.innerHTML = htmlItems.join("");
+  // Set the cart total
+  document.getElementById("cartTotal").textContent = totalCartPrice();
 }
 
 function cartItemTemplate(item) {
@@ -28,6 +32,14 @@ function cartItemTemplate(item) {
     <p class="cart-card__quantity">qty: ${quantity}</p>
     <p class="cart-card__price">$${price}</p>
   </li>`;
+}
+
+
+function totalCartPrice() {
+  const cartItems = getLocalStorage("so-cart") || [];
+  return cartItems.reduce((total, item) => {
+    return total + (item.FinalPrice || 0) * (item.quantity || 1);
+  }, 0).toFixed(2);
 }
 
 renderCartContents();
