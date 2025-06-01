ad2fa5c950a5955baeb34ead15808e1195d36470
dandrichest <dandrichest@gmail.com>
2025-06-01 15:19:38 +0100

    more

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,15 +1,15 @@
-const baseURL = import.meta.env.VITE_SERVER_URL;
-
-function convertToJson(jsonResponse) {
-  if (jsonResponse.ok) {
-    return jsonResponse.json();
+const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
+async function convertToJson(res) {
+  const data = await res.json();
+  if (res.ok) {
+    return data;
   } else {
-    throw { name: "servicesError", message: jsonResponse };
+    throw { name: "servicesError", message: data };
   }
 }
 
 export default class ExternalServices {
-  constructor() {
+  constructor(category) {
     // this.category = category;
     // this.path = `../public/json/${this.category}.json`;
   }
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,14 +13,7 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-// set a listener for both touchend and click
-export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener("click", callback);
-}
+
 
 // get the product id from the query string
 export function getParam(param) {
@@ -63,33 +56,38 @@ export async function loadHeaderFooter() {
   renderWithTemplate(footerTemplate, footerElement);
 }
 
-export function alertMessage(message, scroll = true) {
-  // create element to hold the alert
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
+
+export function alertMessage(message, scroll = true, duration = 3000) {
   const alert = document.createElement("div");
   alert.classList.add("alert");
+  alert.innerHTML = `<p>${message}</p><span>X</span>`;
 
-  // create close button
-  const closeBtn = document.createElement("span");
-  closeBtn.innerText = "X";
-  closeBtn.classList.add("close-btn");
-
-  // set the contents
-  alert.innerText = message;
-  alert.appendChild(closeBtn);
-
-  // add listener to remove alert when close button is clicked
-  alert.addEventListener("click", function(e) {
-      if (e.target.classList.contains("close-btn")) { 
-          this.remove();
-      }
+  alert.addEventListener("click", function (e) {
+    if (e.target.tagName == "SPAN") {
+      main.removeChild(this);
+    }
   });
-
-  // add the alert to the top of main
   const main = document.querySelector("main");
   main.prepend(alert);
+  // make sure they see the alert by scrolling to the top of the window
+  //we may not always want to do this...so default to scroll=true, but allow it to be passed in and overridden.
+  if (scroll) window.scrollTo(0, 0);
 
-  // ensure visibility by scrolling to the top of the window if needed
-  if(scroll) {
-    window.scrollTo(0,0);
-  }
+  // left this here to show how you could remove the alert automatically after a certain amount of time.
+  // setTimeout(function () {
+  //   main.removeChild(alert);
+  // }, duration);
+}
+
+export function removeAllAlerts() {
+  const alerts = document.querySelectorAll(".alert");
+  alerts.forEach((alert) => document.querySelector("main").removeChild(alert));
 }
