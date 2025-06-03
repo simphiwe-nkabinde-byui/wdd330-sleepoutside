da6e8574768bc540267c0f9a1faab4cc02ba6d93
simphiwe-nkabinde-byui <snkabinde@byupathway.edu>
2025-06-03 22:49:35 +0200

    W03 Team Activity: Dynamic Header and Footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
 
 const tentsDataSource = new ProductData("tents");
 const tentsElement = document.querySelector(".product-list");
 const tentsList = new ProductList("tents", tentsDataSource, tentsElement);
 tentsList.init();
+loadHeaderFooter();
