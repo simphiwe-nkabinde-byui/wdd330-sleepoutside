ffc12343b94ea93cb3d935094b1668635b269be2
simphiwe-nkabinde-byui <snkabinde@byupathway.edu>
2025-06-04 02:53:44 +0200

    W03 Individual Task: Report - Product List sort

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,4 +1,4 @@
-import { renderListWithTemplate } from "./utils.mjs";
+import { getParam, renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
   return `
@@ -16,25 +16,61 @@ function productCardTemplate(product) {
     `;
 }
 
+function sortList(list, field) {
+  list.sort((a, b) => {
+    if (!isNaN(a[field]) || !isNaN(b[field])) {
+      return a[field] - b[field];
+    }
+    const nameA = a[field].toUpperCase(); // ignore upper and lowercase
+    const nameB = b[field].toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    // names must be equal
+    return 0;
+  });
+}
+
+function setNewParam(name, value) {
+  let url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+
+  params.set(name, value);
+  window.location.href = `${window.location.origin}${window.location.pathname}?${params.toString()}`;
+}
+
 export default class ProductList {
   constructor(category, dataSource, listElement) {
     this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
+    this.sortButtons = {
+      byName: document.querySelector("#sort-by-name"),
+      byPrice: document.querySelector("#sort-by-price"),
+    };
   }
 
   async init() {
-    const list = await this.dataSource.getData(this.category)
+    const list = await this.dataSource.getData(this.category);
+    const sortField = getParam("sort");
+    if (sortField) {
+      sortList(list, sortField);
+    }
+
+    // addd click event listeners to sort buttons
+    this.sortButtons.byName.addEventListener("click", () =>
+      setNewParam("sort", "NameWithoutBrand"),
+    );
+    this.sortButtons.byPrice.addEventListener("click", () =>
+      setNewParam("sort", "ListPrice"),
+    );
     this.renderList(list);
   }
 
   renderList(list) {
-    // const htmlStrings = list.map(productCardTemplate);
-    // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-
-    // apply use new utility function instead of the commented code above
     renderListWithTemplate(productCardTemplate, this.listElement, list);
-
   }
-
 }
