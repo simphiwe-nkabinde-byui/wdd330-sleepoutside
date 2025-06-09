211d95d8d33dbd3401172d611ed86d13cc22f839
simphiwe-nkabinde-byui <snkabinde@byupathway.edu>
2025-06-09 23:39:54 +0200

    W04 Team Activity: Checkout

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,5 +1,7 @@
 import { getLocalStorage } from "./utils.mjs";
 
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
 export default class CheckoutProcess {
   constructor(key, outputSelector) {
     this.key = key;
@@ -9,6 +11,7 @@ export default class CheckoutProcess {
     this.shipping = 0;
     this.tax = 0;
     this.orderTotal = 0;
+    this.subTotal = 0;
   }
 
   init() {
@@ -18,17 +21,32 @@ export default class CheckoutProcess {
 
   calculateItemSubtotal() {
     // calculate and display the total dollar amount of the items in the cart, and the number of items.
-    const cartIemTotals = this.list.map((i) => i.FinalPrice * i.quantity);
-    this.itemTotal = cartIemTotals.reduce((acc, curr) => acc + curr, 0);
+    this.itemTotal = this.list.length;
+    const numItems = document.querySelector(
+      `${this.outputSelector} #num-items`,
+    );
+    numItems.innerText = this.itemTotal;
+    const cartItemTotals = this.list.map((i) => i.FinalPrice * i.quantity);
+    this.subTotal = cartItemTotals.reduce((acc, curr) => acc + curr, 0);
     const subtotal = document.querySelector(`${this.outputSelector} #subtotal`);
-    subtotal.innerText = `$${this.itemTotal.toFixed(2)}`;
+    subtotal.innerText = `$${this.subTotal.toFixed(2)}`;
   }
   calculateOrderTotal() {
     // calculate the tax and shipping amounts. Add those to the cart total to figure out the order total
-    this.tax = this.itemTotal * 0.06;
+    this.tax = this.subTotal * 0.06;
     this.shipping = 10 + (this.list.length - 1) * 2;
-    this.orderTotal = this.itemTotal + this.tax + this.shipping;
+    this.orderTotal = this.subTotal + this.tax + this.shipping;
     this.displayOrderTotals();
+  }
+
+  // takes the items currently stored in the cart (localstorage) and returns them in a simplified form.
+  packageItems(items) {
+    return items.map((item) => ({
+      id: item.Id,
+      name: item.Name,
+      price: item.FinalPrice,
+      quantity: item.quantity,
+    }));
   }
 
   displayOrderTotals() {
@@ -37,5 +55,46 @@ export default class CheckoutProcess {
     tax.innerText = `$${this.tax.toFixed(2)}`;
     const shipping = document.querySelector(`${this.outputSelector} #shipping`);
     shipping.innerText = `$${this.shipping.toFixed(2)}`;
+    const orderTotal = document.querySelector(
+      `${this.outputSelector} #order-total`,
+    );
+    orderTotal.innerText = `$${this.orderTotal.toFixed(2)}`;
   }
+  async checkout(form) {
+    const formData = new FormData(form);
+    const formObject = {};
+    for (const key of formData.keys()) {
+      formObject[key] = formData.get(key);
+    }
+
+    const packagedItems = this.packageItems(this.list);
+    const payload = {
+      orderDate: new Date().toJSON(),
+      ...formObject,
+      items: packagedItems,
+      orderTotal: this.orderTotal,
+      shipping: this.shipping,
+      tax: this.tax,
+    };
+    console.log({ payload });
+
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    };
+
+    const response = await fetch(`${baseURL}checkout`, options);
+    if (!response.ok) {
+      console.log("error:", response);
+    }
+    const jsonResp = await response.json();
+    console.log({ jsonResp });
+
+    // get the form element data by the form name
+    // convert the form data to a JSON order object using the formDataToJSON function
+    // populate the JSON order object with the order Date, orderTotal, tax, shipping, and list of items
+    // call the checkout method in the ExternalServices module and send it the JSON order data.
+  }
+  formDataToJSON() {}
 }
